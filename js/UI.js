77d23e8154430d530fb75eed09f45e95c93ff26e
Vesa Timonen <vtimonen@gmail.com>
2022-05-26 09:42:50 +0300

    Separate keyboard functionality

--- js/UI.js
+++ js/UI.js
@@ -3,9 +3,9 @@
  *****************************************************************************/
 const gameScreen     = document.getElementById("game-screen");
 const gameBoard      = document.getElementById("game-board");
+
 const buttonRestart  = document.getElementById("button-restart");
 const buttonUndo     = document.getElementById("button-undo");
-
 
 var   gameBoardWidth = gameBoard.clientWidth;
 var   gameBoardCellSize;
@@ -48,58 +48,6 @@ function uiRestart() {
     uiGameRedraw(game);
 }
 
-
-/*****************************************************************************
- * Keyboard input handling
- *****************************************************************************/
-function uiKeyPressed(event) {
-    let move = "";
-    switch (event.key) {
-        case 'ArrowUp':
-        case 'w':
-        case 'W':
-            move = "up";
-            break;
-        case 'ArrowDown':
-        case 's':
-        case 'S':
-            move = "down";
-            break;
-        case 'ArrowLeft':
-        case 'a':
-        case 'A':
-            move = "left";
-            break;
-        case 'ArrowRight':
-        case 'd':
-        case 'D':
-            move = "right";
-            break;
-        case 'Escape':
-        case 'Backspace':
-            uiUndo();
-            return;
-            break;
-        default:
-            return;
-    }
-
-    /* Execute move on game board */
-    if (game.moveExecute(move) == false) {
-        return;
-    }
-
-    /* Redraw board */
-    uiGameRedraw(game);
-}
-
-
-
-
-/*****************************************************************************
- * Register keyboard event handlers
- *****************************************************************************/
-window.addEventListener("keydown",     uiKeyPressed);
 
 /*****************************************************************************
  * Register button event handlers
---
+++ js/UI_keyboard.js
@@ -0,0 +1,49 @@
+
+/*****************************************************************************
+ * Keyboard input handling
+ *****************************************************************************/
+function uiKeyPressed(event) {
+    let move = "";
+    switch (event.key) {
+        case 'ArrowUp':
+        case 'w':
+        case 'W':
+            move = "up";
+            break;
+        case 'ArrowDown':
+        case 's':
+        case 'S':
+            move = "down";
+            break;
+        case 'ArrowLeft':
+        case 'a':
+        case 'A':
+            move = "left";
+            break;
+        case 'ArrowRight':
+        case 'd':
+        case 'D':
+            move = "right";
+            break;
+        case 'Escape':
+        case 'Backspace':
+            uiUndo();
+            return;
+            break;
+        default:
+            return;
+    }
+
+    /* Execute move on game board */
+    if (game.moveExecute(move) == false) {
+        return;
+    }
+
+    /* Redraw board */
+    uiGameRedraw(game);
+}
+
+/*****************************************************************************
+ * Register keyboard event handlers
+ *****************************************************************************/
+window.addEventListener("keydown",     uiKeyPressed);
