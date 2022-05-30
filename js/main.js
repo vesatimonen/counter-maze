8ce9b15a5d34f7ab224db991fffc2945ff770204
Vesa Timonen <vtimonen@gmail.com>
2022-05-30 10:21:16 +0300

    Fine-tune challenges

--- js/main.js
+++ js/main.js
@@ -31,37 +31,36 @@ function levelStart(level) {
  * Game levels
  *****************************************************************************/
 var gameLevels = [
+    {width: 3, height: 3, moves:  2},
     {width: 3, height: 3, moves:  4},
     {width: 3, height: 3, moves:  6},
     {width: 3, height: 3, moves:  8},
     {width: 3, height: 3, moves: 10},
     {width: 3, height: 3, moves: 12},
 
-    {width: 4, height: 4, moves: 14},
-    {width: 4, height: 4, moves: 16},
-    {width: 4, height: 4, moves: 18},
+    {width: 3, height: 4, moves: 14},
+    {width: 3, height: 4, moves: 16},
+    {width: 3, height: 4, moves: 18},
+
     {width: 4, height: 4, moves: 20},
     {width: 4, height: 4, moves: 22},
     {width: 4, height: 4, moves: 24},
 
-    {width: 5, height: 5, moves: 26},
-    {width: 5, height: 5, moves: 28},
-    {width: 5, height: 5, moves: 30},
+    {width: 4, height: 5, moves: 26},
+    {width: 4, height: 5, moves: 28},
+    {width: 4, height: 5, moves: 30},
+
     {width: 5, height: 5, moves: 32},
     {width: 5, height: 5, moves: 34},
     {width: 5, height: 5, moves: 36},
+    {width: 5, height: 5, moves: 38},
 
-    {width: 6, height: 6, moves: 38},
-    {width: 6, height: 6, moves: 40},
-    {width: 6, height: 6, moves: 42},
-    {width: 6, height: 6, moves: 44},
-    {width: 6, height: 6, moves: 46},
-    {width: 6, height: 6, moves: 48},
-    {width: 6, height: 6, moves: 50},
-    {width: 6, height: 6, moves: 52},
+    {width: 5, height: 6, moves: 40},
+    {width: 5, height: 6, moves: 42},
+    {width: 5, height: 6, moves: 44},
+    {width: 5, height: 6, moves: 46},
 
-    {width: 7, height: 7, moves: 54},
-
+    {width: 5, height: 7, moves: 48},
 ];
 
 /*****************************************************************************
